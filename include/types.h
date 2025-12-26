d448ba538134dc57423a3a9a2898f20462db23b0
Goofygiraffe06 <rahul.narsing06@gmail.com>
2025-12-26 09:14:06 +0530

    add all header field types

--- include/types.h
+++ include/types.h
@@ -5,7 +5,29 @@
 #include <stddef.h>
 
 typedef struct {
-  uint8_t magic[16]; // The header string: "SQLite format 3\000"
+  uint8_t magic[16];
+  uint16_t page_size;
+  uint8_t file_format_write;
+  uint8_t file_format_read;
+  uint8_t reserved_space;
+  uint8_t max_embed_payload_frac;
+  uint8_t min_embed_payload_frac;
+  uint8_t leaf_payload_frac;
+  uint32_t file_change_counter;
+  uint32_t header_db_size;
+  uint32_t first_freelist_trunk;
+  uint32_t total_freelist_trunk;
+  uint32_t schema_cookie;
+  uint32_t schema_format_number;
+  uint32_t default_page_cache_size;
+  uint32_t page_number_largest_root;
+  uint32_t db_text_encoding;
+  uint32_t user_version;
+  uint32_t incremental_version_mode;
+  uint32_t application_id;
+  uint8_t reserved_expansion[20];
+  uint32_t version_valid_for;
+  uint32_t sqlite_version_number;
 } db_header_t;
 
 #endif
